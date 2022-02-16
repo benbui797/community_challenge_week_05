70a2e66bc3e19d7125dce575476f314022d12bec
benbui797 <95748001+benbui797@users.noreply.github.com>
2022-02-16 21:55:10 +0100

    Add files via upload
    Added block logic and optimised row and column logics

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,14 +1,14 @@
 import pandas as pd
-import requests
+# import requests
 import os
 
 def clear():
     os.system('cls')
 
-url = "https://community-challenge.netlify.app/data/sudoku2Solve.json"
-r2 = requests.get(url)
-json_file2 = r2.json()
-sudoku_solve = json_file2["sudokuSolve"]
+# url = "https://community-challenge.netlify.app/data/sudoku2Solve.json"
+# r2 = requests.get(url)
+# json_file2 = r2.json()
+# sudoku_solve = json_file2["sudokuSolve"]
 
 df_medium = pd.read_csv("sudoku_medium.csv", header=None)
 df_hard = pd.read_csv("sudoku_hard.csv", header=None)
@@ -28,65 +28,92 @@ def find_missing(numbers_list):
     return missing_list
 
 def find_easy_row(grid):
-    frow_count = 9
     frow = None
+    frow_count = 9
+    frow_values = None
+
     for r in range(9):
         if grid[r].count(0) != 0 and grid[r].count(0) < frow_count:
             frow = r
-            frow_count = grid[frow].count(0)       
-    for c in range(9):
-        if grid[frow][c] == 0:
-            guesslist = find_missing(grid[frow])
-            return frow, c, guesslist
-    return None, None, None
+            frow_count = grid[frow].count(0)
+            frow_values = grid[frow]
+    return frow, frow_count, frow_values
 
 def find_easy_column(grid):
-    fcol_count = 9
     fcol = None
+    fcol_count = 9
+    fcol_values = None
+
     for c in range(9):
         col_values = [grid[i][c] for i in range(9)]
         if col_values.count(0) != 0 and col_values.count(0) < fcol_count:
             fcol = c
             fcol_count = col_values.count(0)
-    col_values = [grid[i][fcol] for i in range(9)]
+            fcol_values = col_values
+    return fcol, fcol_count, fcol_values
+
+def find_easy_block(grid):
+    fblockrows = None
+    fblockcols = None
+    fblock_count = 9
+    fblock_values = None
     
-    for r in range(9):
-        if grid[r][fcol] == 0:
-            guesslist = find_missing(col_values)
-            return r, fcol, guesslist
+    for block_r_start in range(0, 9, 3):
+        for block_c_start in range(0, 9 ,3):
+            block_values = [grid[r][c] for r in range(block_r_start, block_r_start + 3) for c in range(block_c_start, block_c_start +3)]
+            block_rows = [r for r in range(block_r_start, block_r_start + 3)]
+            block_cols = [c for c in range(block_c_start, block_c_start + 3)]
+            if block_values.count(0) != 0 and block_values.count(0) < fblock_count:
+                fblock_count = block_values.count(0)
+                fblockrows = block_rows
+                fblockcols = block_cols
+                fblock_values = block_values
+    return fblockrows, fblockcols, fblock_count, fblock_values
+
+def get_block_values(grid, row, col):
+    row_start = (row // 3) * 3
+    col_start = (col // 3) * 3
+    block_values = [grid[r][c] for r in range(row_start, row_start + 3) for c in range(col_start, col_start +3)]
+    return block_values
 
 def find_easy_cell(grid):
-    frow_count = 9
-    frow = None
-    fcol_count = 9
-    fcol = None
-
-    # check which row is most filled
-    for r in range(9):
-        if grid[r].count(0) != 0 and grid[r].count(0) < frow_count:
-            frow = r
-            frow_count = grid[frow].count(0) 
-    # check which column is most filled
-    for c in range(9):
-        col_values = [grid[i][c] for i in range(9)]
-        if col_values.count(0) != 0 and col_values.count(0) < fcol_count:
-            fcol = c
-            fcol_count = col_values.count(0)             
-
+    frow, frow_count, frow_values = find_easy_row(grid)
+    fcol, fcol_count, fcol_values = find_easy_column(grid)
+    fblockrows, fblockcols, fblock_count, fblock_values = find_easy_block(grid)
+    
     # if all rows are full, return None
     if frow == None:
         return None, None, None
-    elif frow_count >= fcol_count:
-        for c in range(9):
-            if grid[frow][c] == 0:
-                guesslist = find_missing(grid[frow])
-                return frow, c, guesslist   
-    elif frow_count < fcol_count:
+    
+    # if block is most filled, get missing values from block. 
+    # Get first empty cell in block and get missing values for that row and column.
+    #
+    # elif column is most filled, get missing values for column, row and empty cell block.
+    #
+    # elif row is most filled, get missing values for column, row and empty cell block.
+
+    if fblock_count < fcol_count and fblock_count < frow_count:
+        for r in fblockrows:
+            for c in fblockcols:
+                if grid[r][c] == 0:
+                    col_values = [grid[i][c] for i in range(9)]
+                    row_values = grid[r]
+                    guesslist = find_missing(fblock_values + col_values + row_values)
+                    return r, c, guesslist
+    elif fcol_count < frow_count:
         for r in range(9):
             if grid[r][fcol] == 0:
-                col_values = [grid[i][fcol] for i in range(9)]
-                guesslist = find_missing(col_values)
+                row_values = grid[r]
+                block_values = get_block_values(grid, r, fcol)
+                guesslist = find_missing(fcol_values + row_values + block_values)
                 return r, fcol, guesslist
+    elif frow_count <= fcol_count:
+        for c in range(9):
+            if grid[frow][c] == 0:
+                block_values = get_block_values(grid, frow, c)
+                col_values = [grid[i][c] for i in range(9)]
+                guesslist = find_missing(frow_values + block_values + col_values)
+                return frow, c, guesslist
     return None, None, None
 
 def find_empty_cell(grid):
@@ -96,7 +123,7 @@ def find_empty_cell(grid):
                 return r, c
     return None, None
 
-def check_guess(grid, guess, row, col, x_sudoku):
+def check_guess(grid, guess, row, col, x_sudoku=False):
     
     # Check if guess is in the same row
     row_values = grid[row]
@@ -129,7 +156,7 @@ def check_guess(grid, guess, row, col, x_sudoku):
                 return False
     return True
 
-def solve_sudoku(grid, x_sudoku=False):
+def solve_sudoku(grid):
     # row, col = find_empty_cell(grid)
     row, col, guesslist = find_easy_cell(grid)
     if row is None:
@@ -139,7 +166,7 @@ def solve_sudoku(grid, x_sudoku=False):
     for guess in guesslist:
         global counter
         counter += 1
-        if check_guess(grid, guess, row, col, x_sudoku):
+        if check_guess(grid, guess, row, col):
             grid[row][col] = guess
             # clear()
             # grid_print(grid)
@@ -174,4 +201,6 @@ def solve_start(sudoku):
     else:
         print("Could not find a solution")
 
-solve_start(sudoku_solve)
+# solve_start(sudoku_solve)
+
+solve_start(sudoku_evil2)
